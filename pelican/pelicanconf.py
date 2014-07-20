43672d78ed66707b1dbd7756cac233d06cba93ea
lostman-github <lostman.worlds.end+github@gmail.com>
2014-07-20 11:44:30 +0900

    change date format

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -34,4 +34,4 @@ ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}.html'
 USE_FOLDER_AS_CATEGORY = False
 DEFAULT_CATEGORY = 'misc'
 
-DEFAULT_DATE_FORMAT = '%Y/%m/%d %a'
+DEFAULT_DATE_FORMAT = '%Y/%m/%d (%a)'
